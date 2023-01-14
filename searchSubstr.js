83de85835cda572e98932c95ba1051b2cfafb20a
RomanButsko <75183199+RomanButsko@users.noreply.github.com>
2023-01-14 21:33:16 +0300

    search substring

---
+++ searchSubstr.js
@@ -0,0 +1,10 @@
+function searchSubstr(mainStr, subStr) {
+    if (subStr.length > mainStr.length) return false;
+    for (let i = 0; i < mainStr.length; i++) {
+        for (let j = 0; j < subStr.length; j++) {
+            if (mainStr[i + j] !== subStr[j]) break;
+            if (j === subStr.length - 1) return true;
+        }
+    }
+    return false;
+}
